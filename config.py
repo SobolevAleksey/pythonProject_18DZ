7319faaabd7273890d4810ed23ec973f8729f184
Aleksey Sobolev <113844347+SobolevAleksey@users.noreply.github.com>
2023-02-23 11:00:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config: # сюда зачем то передается object
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:' # 'sqlite:///./movies.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:' # 'sqlite:///./movies.db' перетащить базу в папку instance
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
