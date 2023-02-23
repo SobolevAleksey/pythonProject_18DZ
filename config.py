badc419900ce329a2523154646231f113413da6c
Aleksey Sobolev <113844347+SobolevAleksey@users.noreply.github.com>
2023-02-23 15:37:56 +0300

    change memory

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 class Config: # сюда зачем то передается object
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:' # 'sqlite:///./movies.db' перетащить базу в папку instance
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///./movies.db'
+    # 'sqlite:///:memory:' перетащить базу в папку instance
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
-#     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:' 'sqlite:///movies.db'
