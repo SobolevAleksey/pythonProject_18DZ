99ba4b94cb2f565760dea07fb7f1d3669af39b2c
Aleksey Sobolev <113844347+SobolevAleksey@users.noreply.github.com>
2023-02-23 09:45:41 +0300

    Update container.py

--- container.py
+++ container.py
@@ -6,11 +6,11 @@ from services.genre import GenreService
 from services.movie import MovieService
 from setup_db import db
 
-movie_dao = MovieDAO(db)
+movie_dao = MovieDAO(db.session)
 movie_service = MovieService(movie_dao)
 
-director_dao = DirectorDAO(db)
+director_dao = DirectorDAO(db.session)
 director_service = DirectorService(director_dao)
 
-genre_dao = GenreDAO(db)
+genre_dao = GenreDAO(db.session)
 genre_service = GenreService(genre_dao)
